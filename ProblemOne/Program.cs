f71c753119376fb00bd49eb4b2c29f20f0623c37
HP <HP@ABIR-MOHAMMAD>
2021-02-07 20:29:10 +0600

    ProblemOne Committed

---
+++ ProblemOne/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemOne
+{
+  class Even
+        {
+            static void Main()
+            {
+                int N, i;
+                Console.WriteLine("Enter the Countable even Number : ");
+                N = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("----------------------------------");
+
+                for (i = 1; i <= N; i++)
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+
+                    
+                Console.ReadLine();
+            }
+
+        }
+    
+
+}
